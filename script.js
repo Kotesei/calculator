d0edd788426a61c3c94e5ec620cd58858dc56d44
Kotesei <192398548+Kotesei@users.noreply.github.com>
2025-01-10 10:58:23 -0500

    working on percentage and discovering bugs.

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ let num;
 let num2;
 let total;
 let doneOperating;
+let isPercentage;
+let savePercent;
 
 function loadBtns() {
 let thisBtn;
@@ -65,15 +67,17 @@ for (let btn = 0; btn < btns.length; btn++) {
                             invertNumber();
                             if (num && !num2) {
                                 num = results.innerHTML;
-                                console.log(num, num2);
                             }
                             else {
                                 num2 = results.innerHTML
-                                console.log(num, num2);
                             }
                         break;
                         case "%":
                             if (sizeLimit) return
+                            if (results.innerHTML === "0") return
+
+                            // Build a function for this 
+                            percentage();
                         break;
                     }
                 })
@@ -85,6 +89,7 @@ for (let btn = 0; btn < btns.length; btn++) {
 
                     // For operation add, multi, divide, sub
                     if (e.target.innerHTML !== "=") {
+                        console.log(isPercentage);
                         
                         if (isOperating) {
                             if (!num) num = 0
@@ -106,13 +111,22 @@ for (let btn = 0; btn < btns.length; btn++) {
                             results.innerHTML = "0"
                         }
                         doneOperating = false;
+                        isPercentage = false;
                         operation = e.target.innerHTML  
                     }
 
                     // For pressing equal
                     if (e.target.innerHTML === "=") {
+                        console.log(num, num2, total);
                         if (doneOperating) {
                             num = total
+                            if (isPercentage) {
+                                console.log("is percentage");
+                                num2 = num * savePercent / 100
+                            } else {
+                            console.log("not percentage");
+                            }
+                            
                         }
                         // Return num as the total if no num2 found
                         if (!num) num = 0
@@ -133,6 +147,20 @@ for (let btn = 0; btn < btns.length; btn++) {
 
 
 
+function percentage() {
+    if (isOperating) {
+        num2 = num * num2 / 100 
+        console.log(num2);
+    } else {
+        num = results.innerHTML / 100;
+    }
+    savePercent = results.innerHTML;
+    results.innerHTML = results.innerHTML + "%"
+    console.log(savePercent);
+    isPercentage = true;
+}
+
+
 loadBtns();
 
 function calc(num, operation, num2) {
@@ -142,10 +170,8 @@ function calc(num, operation, num2) {
         case "-":
         return Number(num) - Number(num2);
         case "/":
-            // if (num2 === "0") num2 = 1
         return Number(num) / Number(num2);
         case "*":
-            // if (num2 === "0") num2 = 1
         return Number(num) * Number(num2);
     }
 }
@@ -192,6 +218,7 @@ function clear() {
             console.log("Wipe");
             isOperating = false;
             doneOperating = false;
+            isPercentage
             num = undefined;
             num2 = undefined;
                 results.innerHTML = "0"
@@ -199,6 +226,7 @@ function clear() {
             if (doneOperating) {
                 console.log("Wipe");
                 isOperating = false;
+                isPercentage
             doneOperating = false;
                 num = undefined;
                 num2 = undefined;
@@ -215,6 +243,7 @@ function clear() {
         console.log("Wipe");
         isOperating = false;
             doneOperating = false;
+            isPercentage
         num = undefined;
         num2 = undefined;
         results.innerHTML = "0"
@@ -227,369 +256,3 @@ function invertNumber() {
     if (results.innerHTML.includes("-")) results.innerHTML = results.innerHTML.split("-")[1]
     else results.innerHTML = "-" + results.innerHTML
 }
-
-
-
-// Saves (User Input: Step 1 of 2)
-// let input;
-
-// let isNegative;
-
-// Stores the first value
-// let num;
-
-// Saves the last operator except equal
-// let lastUsedOperator;
-
-// Stores the second value
-// let num2;
-// let storeNum2
-
-// After num, lastUsedOperator, and num2 have values run the functions and store it in total
-// let total;
-
-// let isInputting;
-
-// Enable this after pressing equal 
-// let operationDone;
-
-// Specifically for the equal sign repeats the last action
-// let isRepeating;
-
-// Specifically for every other operator, takes total and turns it to num instead
-// let isContinuing
-
-// For the results not to go outside the div if it gets too big
-// let sizeLimit;
-
-
-
-
-// function resizeResult(isReset) {
-//     const getContainerWidth = Number(window.getComputedStyle(resultContainer).width.split("px")[0])
-//     const getContainerPadding = Number(window.getComputedStyle(resultContainer).padding.split("px")[0] * 2)
-//     const getTextWidth = Number(window.getComputedStyle(results).width.split("px")[0])
-//     const getTextFontSize = Number(window.getComputedStyle(results).fontSize.split("px")[0] / 16)
-
-//     let fontSize;
-    
-//     if (isReset) {
-//         fontSize = originalfontSize
-//         results.style.fontSize = `${fontSize}rem`
-//         isReset = false
-//     }
-
-    
-//     if (fontSize === undefined) fontSize = getTextFontSize;
-
-//     if (fontSize === 3) {
-//         console.log("Reached Size Limit");
-//         sizeLimit = true;
-//     }
-    
-
-//     if (getTextWidth + getContainerPadding > getContainerWidth) {
-//         fontSize--
-//         results.style.fontSize = `${fontSize}rem`
-//         resizeResult();
-//     }
-
-
-// }
-
-// function loadBtns() {
-//     let thisBtn;
-// const btns = ["AC", "+/-", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="]
-// const numerics = ".0123456789"
-// const utility = ["AC", "+/-", "%"]
-// const operations = ["/", "*", "-", "+", "="]
-
-// for (let btn = 0; btn < btns.length; btn++) {
-//     btnsContainer.insertAdjacentHTML("beforeend", `<button class="btn">${btns[btn]}</button>`)    
-//     thisBtn = document.querySelectorAll(".btn")[btn]
-//     if (thisBtn.innerHTML === "0") {
-//         thisBtn.style.flex = "1"
-//     }
-
-//     // Move all the event listeners inside here instead maybe a helper function? 
-//     if (numerics.includes(thisBtn.innerHTML)) {
-//         thisBtn.classList.add("color--numerics")
-//     }
-//     if (utility.includes(thisBtn.innerHTML)) {
-//         thisBtn.classList.add("color--utility")
-//     }
-//     if (operations.includes(thisBtn.innerHTML)) {
-//         thisBtn.classList.add("color--operators")
-
-//     }
-
-//     thisBtn.addEventListener("click", function(el) {
-        
-//         // This button is for the clearing
-//             if (el.target.innerHTML.includes("AC")) {
-//                 // Clear set to true (and if pressed again then wipes this is why we use a number rather than true / false)
-//                 clear(1);
-//             }
-//             if (el.target.innerHTML.includes("+/-")) turnToNegative();
-
-//             if (el.target.innerHTML.includes("%")) percentage();
-        
-//         // Checks if button pressed is a numeric one and if the sizeLimit hasn't been reached.
-//         if (el.target.classList.contains("color--numerics") && !sizeLimit) {
-//             clear(0)
-//             operationDone = false;
-//             isInputting = true;
-//             if (input === undefined || input === "0") input = el.target.innerHTML
-//             else
-//             input += el.target.innerHTML
-//         results.innerHTML = input
-//     }
-    
-//     // Sets the operator to use
-//     if (el.target.classList.contains("color--operators")) {
-//         isInputting = false;
-//             clear(0)      
-//             // Second input
-//             if (num !== undefined) {
-//                 operate(el.target.innerHTML, true);
-//             } else {
-//                 // First input
-//                 operate(el.target.innerHTML);
-//             }
-//         }
-
-//         resizeResult();
-//     })
-// }
-
-// }
-
-
-// // Fix this code up
-
-
-// // Saves the last operator except equal
-// // let lastUsedOperator;
-
-// // Enable this after pressing equal 
-// // let operationDone;
-
-// // Specifically for the equal sign repeats the last action
-// // let isRepeating;
-
-// // Specifically for every other operator, takes total and turns it to num instead
-// // let isContinuing
-
-// // Enable this after pressing equal 
-// // operationDone = true;
-// // If true then repeat operation based off of lastUsedOperator 
-
-// function operate(operator) {
-//     num ??= input
-// if (operator === "=") {
-//     if (!num) total = 0
-//     // If operationDone then repeat last one
-//     if (operationDone) {
-//         getTotal("Repeat")
-//     } else {
-//         getTotal();
-//         isContinuing = false;
-//     }
-//     operationDone = true;
-//     updateScreen()
-// }
-// if (operator !== "=") {
-//     // If operationDone then turn num into total and getTotal
-//     if (operationDone) {
-//         console.log("Continue after pressing equal");
-//         operationDone = false
-//     }
-//     if (isContinuing) {
-//         console.log(num, num2, total);
-//         getTotal("Continue", operator)
-//         console.log(num, num2, total);
-        
-//         operationDone = false
-//     }
-//     lastUsedOperator = operator
-//     updateScreen()
-//     isContinuing = true;
-// }
-
-// }
-
-// function updateScreen() {
-//     if (isContinuing) input = total
-//     if (operationDone) input = total
-//     if (!total) input = "0"
-//     results.innerHTML = input
-//     input = "0"
-//     }
-
-
-
-// function getTotal(isDuring, operator) {
-//     num2 ??= input
-//     console.log(num, num2, total);
-//     if (isDuring === "Repeat") {
-//         console.log("Repeat Operation!");
-//         if (isPercent) console.log("Test");
-//         num = total
-//         switch (lastUsedOperator) {
-//             case "+":
-//                 total = add(total, storeNum2)
-//                 break;
-//                 case "-":
-//                     total = subtract(total, storeNum2)
-//                     break;
-//                     case "*":
-//                         total = multiply(total, storeNum2)
-//                         break;
-//                         case "/":
-//                             total = divide(total, storeNum2)
-//                             break;
-//                         }
-//                     } else if (!isDuring){
-//                         console.log("Results!");
-//                         storeNum2 = num2
-//                         if (!isPercent) num2 = input
-//                         if (!lastUsedOperator) total = input
-//                         if (isPercent) total = num;
-//                         switch (lastUsedOperator) {
-//             case "+":
-//                 total = add(num, num2)
-//                 break;
-//             case "-":
-//                 total = subtract(num, num2)
-//                 break;
-//             case "*":
-//                 total = multiply(num, num2)
-//                 break;
-//             case "/":
-//                 total = divide(num, num2)
-//                 break;
-//         }
-        
-//     } else if (isDuring === "Continue") {
-//         console.log("Continue Operation!");
-//         console.log(lastUsedOperator, operator);
-//         switch (lastUsedOperator) {
-//                 case "+":
-//                     total = add(num, num2)
-//                     break;
-//                 case "-":
-//                     total = subtract(num, num2)
-//                     break;
-//                 case "*":
-//                     // Set to 1 so it doesn't change
-//                     if (input === "0") num2 = 1
-//                     total = multiply(num, num2)
-//                     break;
-//                 case "/":
-//                     // Set to 1 so it doesn't change
-//                     if (input === "0") num2 = 1
-//                     total = divide(num, num2)
-//                     break;
-//                 }
-//             }
-//             num = total
-    
-//     }
-
-
-// // When page loads
-// loadBtns();
-
-
-// // Should only run after computing
-// // resizeResult();
-
-// function add(val, val2) {
-//     return Number(val) + Number(val2)
-// }
-
-// function subtract(val, val2) {
-//     return Number(val) - Number(val2)
-// }
-
-// function multiply(val, val2) {
-//     return Number(val) * Number(val2)
-// }
-
-// function divide(val, val2) {
-//     return Number(val) / Number(val2)
-// }
-
-
-// // Should double tap to clear all, one tap clears the 2nd number, if 2nd number undefined then clear all
-// function clear(reset) {
-    
-//     if (reset === 1) {
-//         sizeLimit = false;
-//         resizeResult(true)
-//     if (clear.num === undefined) {
-//         clear.num = 0;
-//     }
-//     clear.num += 1
-//     if (clear.num === 1 && !operationDone) {
-//         console.log('Clear Once');
-//         if (num !== undefined) {
-//             num2 = undefined
-//         } else {
-//             num = undefined
-//         }
-
-//         updateScreen();
-//     } else if (clear.num === 2 || operationDone) {
-//         num = undefined;
-//         num2 = undefined;
-//         total = undefined;
-//         isContinuing = false
-//         operationDone = false
-//         updateScreen();
-//         console.log('Wipe');
-//     }
-// } else {
-//     clear.num = 0
-// }
-// }
-
-// let storePositive;
-// // Just take the current input and switch between negative or positive
-// function turnToNegative() {
-//     if (!isInputting || results.innerHTML === "0") return
-//     if (Number(results.innerHTML) < 0) isNegative = true
-//     else isNegative = false
-
-
-//     if (isNegative) {
-//         isNegative = false
-//         storePositive ??= results.innerHTML.split("-")[1]
-//         input = `${storePositive}`
-//         console.log("Turn to positive");
-//         console.log(num, num2, total);
-//     } else
-//     if (!isNegative) {
-//         isNegative = true
-//         storePositive = input
-//         input = `-${input}`
-//         console.log(num, num2, total);
-//         console.log("Turn to negative");
-//     }
-// results.innerHTML = input
-// }
-
-// // Should turn current input into a percentage. If user just presses equal after percentage then just give divide by 100
-// // Example: 300% = 3.00
-// let isPercent;
-// let lastPercent;
-// function percentage() {
-//     console.log(isContinuing);
-//     if (isContinuing) num2 = (num * (input / 100)).toFixed(2)
-//         else num = input / 100
-//     isPercent = true;
-//     results.innerHTML = `${results.innerHTML}%`
-//     console.log(num, num2);
-//     console.log(input);
-// }
-
