15801cdeb0b097d5de73f15519f13f2d34530946
Kotesei <192398548+Kotesei@users.noreply.github.com>
2025-01-14 00:38:47 -0500

    Finished fixing bugs Tested with more complex operations such as:
    500 / -10% + -0.5 + 0.01%
    
    -75 - 50% / -25% + 1%
    
    Always does the multiplcation and division seperately kind of like pemdas.
    This is more a mathematical solving method rather than the traditional calculator solving.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 'use strict'
 const btnsContainer = document.querySelector(".calc--btns")
 const resultContainer = document.querySelector(".calc--input")
-
-// Display (User Input: Step 2 of 2)
 const results = resultContainer.querySelector("p");
 
 
@@ -20,8 +18,10 @@ let total;
 let isFirst;
 let isSecondary;
 let doneOperating;
+let repeatOperation;
 let isPercentage;
 let savePercent;
+let preventChangingOperation;
 
 function loadBtns() {
 let thisBtn;
@@ -40,11 +40,13 @@ for (let btn = 0; btn < btns.length; btn++) {
     if (numerics.includes(thisBtn.innerHTML)) {
                 thisBtn.classList.add("color--numerics")
                 thisBtn.addEventListener("click", function(e) {
+                    preventChangingOperation = false;
+            
                     if (sizeLimit) return
                     if (doneOperating) clear();
                     
                     if (isOperating) {
-                        console.log(num);
+                    
                         isSecondary = true;
                         isFirst = false
                         if (!num2 && e.target.innerHTML === "0") return
@@ -73,21 +75,26 @@ for (let btn = 0; btn < btns.length; btn++) {
                             clear();
                         break;
                         case "+/-":
+                            if (doneOperating) return
                             if (sizeLimit) return
+                            if (results.innerHTML === "0") return
                             invertNumber();
+                            resizeResult()
                             if (num && !num2) {
                                 num = results.innerHTML;
                             }
                             else {
                                 num2 = results.innerHTML
                             }
-                        break;
-                        case "%":
-                            if (sizeLimit) return
+                            break;
+                            case "%":
+                                if (doneOperating) return
+                                if (sizeLimit) return
                             if (results.innerHTML === "0") return
 
                             // Build a function for this 
                             percentage();
+                            resizeResult()
                         break;
                     }
                 })
@@ -99,26 +106,25 @@ for (let btn = 0; btn < btns.length; btn++) {
 
                     // For operation add, multi, divide, sub
                     if (e.target.innerHTML !== "=") {
-                        
-                        // console.log(operation, previousoperation);
-                        if (operation === "*" && !doneOperating && previousoperation) {
-                            if (previousoperation !== "*") {
-                                console.log(operation, previousoperation);
-                                num = calc(num - previousnum2, previousoperation, previousnum2 * num2)
-                            } else {
-                     
-                            total = calc(num, operation, num2)
-                            if (operation === "*" && num2 === 0) total = num
-                            num = total
-                            total = Number(total.toPrecision(5))
-                        }
+                        if (preventChangingOperation) return                        
+                        preventChangingOperation = true;
+                        doneOperating = false;
+                        if (operation === "*" && !doneOperating && previousoperation || operation === "/" && !doneOperating && previousoperation) {
+                            console.log("test");
+                            if (operation === "/" || operation === "*") {
+                                num = previousnum
+                                num2 = calc(previousnum2, operation, num2)
+                                previousnum2 = num2
+                                total = Math.floor(calc(num, previousoperation, num2) * 10000) / 10000
+                                num = total
+                            }
+                          
                         
                         results.innerHTML = total
                         num2 = 0
                         isPercentage = false;
                         operation = e.target.innerHTML  
                         } else {
-                            doneOperating = false;
                         previousnum = num;
                         previousnum2 = num2;
                         
@@ -132,11 +138,11 @@ for (let btn = 0; btn < btns.length; btn++) {
                         } else {
                             total = calc(num, operation, num2)
                             if (operation === "*" && num2 === 0) total = num
-                            total = Number(total.toPrecision(5))
+                            total = Math.floor(total * 10000) / 10000
                             results.innerHTML = total
                             num = total
                             num2 = 0
-                            console.log(e.target.innerHTML);
+                         
                             // if (e.target.innerHTML === "*")
                         }
                         }
@@ -147,37 +153,64 @@ for (let btn = 0; btn < btns.length; btn++) {
                             isOperating = true;
                             results.innerHTML = "0"
                         }
-                        doneOperating = false;
                         isPercentage = false;
                         operation = e.target.innerHTML  
                     }
                 }
                     // For pressing equal
                     if (e.target.innerHTML === "=") {
-                        console.log(num, num2, total);
+                        preventChangingOperation = false;
+                        isOperating = false;
                         if (doneOperating) {
+                            
+                            repeatOperation = true;
+                 
                             num = total
-                            if (isPercentage) {
-                                console.log("is percentage");
-                                console.log(num, num2, total);
-                                if (operation !== "*" || operation !== "/") num2 = num * savePercent / 100
-                            }
-                        }
-                        // Return num as the total if no num2 found
+                            num2 = previousnum2
+                            
+                        } 
+
+                  
+                        if (!repeatOperation) {
+                            if (operation === "*" && !doneOperating && previousoperation || operation === "/" && !doneOperating && previousoperation) {
+                                if (operation === "/" || operation === "*") {
+                                
+                                    num = previousnum
+                                    num2 = calc(previousnum2, operation, num2)
+                              
+                                    previousnum2 = num2
+                                    total = Math.floor(calc(num, previousoperation, num2) * 10000) / 10000
+                                    operation = previousoperation
+                                }
+                    }
+                }
+
+                                 // Return num as the total if no num2 found
                         if (!num) num = 0
                         if (!num2) num2 = 0
-                        console.log(previousoperation);
-                        console.log(operation);
+                    
                         // Calculates the total
                         total = calc(num, operation, num2)
+                        num = total
+                        
+                     
                         if (operation === "*" && num2 === 0) total = num
-                    if (operation) total = Number(total.toPrecision(5))
+                    if (operation) total = Math.floor(total * 10000) / 10000
                         if (!isFinite(calc(num, operation, num2))) total = Infinity
-                        if (!operation || total === undefined) total = num
-
+                        if (!operation || total === undefined) {
+                            num = results.innerHTML
+                            total = num
+                        }
+                    
                         results.innerHTML = total
+                        
+                     
                         doneOperating = true;
+                        previousnum2 = num2
+                        num2 = 0;
 
+                        console.log(num);
+                        console.log(previousnum2);
                     }
                     resizeResult()
                 }
@@ -232,7 +265,7 @@ function resizeResult(isReset) {
     if (fontSize === undefined) fontSize = getTextFontSize;
 
     if (fontSize === 3) {
-        // results.innerHTML = "Size Limit!"
+        results.innerHTML = "Size Limit!"
         sizeLimit = true;
     }
     
@@ -251,7 +284,7 @@ function clear() {
     if (isOperating){
         if (!num2) {
             // Wipes if no num2 found
-            console.log("Wipe");
+       
             isOperating = false;
             doneOperating = false;
             isPercentage = false;
@@ -260,12 +293,13 @@ function clear() {
             total = undefined;
             previousnum = undefined;
             previousoperation = undefined
+            operation = undefined
             
             results.innerHTML = "0"
-            console.log("test case2");
+       
         } else {
             if (doneOperating) {
-                console.log("Wipe");
+               
                 isOperating = false;
                 isPercentage = false;
                 doneOperating = false;
@@ -276,20 +310,20 @@ function clear() {
                 total = undefined;
                 previousnum = undefined;
                 previousoperation = undefined
+                operation = undefined
                 savePercent = undefined
                 results.innerHTML = "0"
-                console.log("test case1");
+          
                
             }
             else {
-                console.log(num2);
-                console.log("Clear Num2");
+              
                 num2 = undefined
                 results.innerHTML = "0"
             }
         }
     } else {
-        console.log("Wipe");
+
         isOperating = false;
         doneOperating = false;
         isPercentage = false
@@ -298,14 +332,20 @@ function clear() {
         total = undefined;
         previousnum = undefined;
         previousoperation = undefined;
+        operation = undefined
         results.innerHTML = "0"
-        console.log("test case3");
+
     }
     resizeResult(true)
 }
 
 function invertNumber() {
-    if (results.innerHTML === "0") return
-    if (results.innerHTML.includes("-")) results.innerHTML = results.innerHTML.split("-")[1]
-    else results.innerHTML = "-" + results.innerHTML
+    if (isPercentage) {
+        results.innerHTML = "-" + Number(results.innerHTML.split("%")[0]) / 100
+        isPercentage = false;
+    } else {
+        if (results.innerHTML.includes("-")) results.innerHTML = results.innerHTML.split("-")[1]
+        else results.innerHTML = "-" + results.innerHTML
+    }
+    
 }
